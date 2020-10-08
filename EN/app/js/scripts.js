56943d90112f8cae9f6bda27777a16d2b8b4b54c
unknown <timpriadko@gmail.com>
2020-10-08 18:08:14 +0300

    first commit

---
+++ EN/app/js/scripts.js
@@ -0,0 +1,91 @@
+'use strict';
+
+$(document).ready(function () {
+  // form-input
+  $('input').focus(function () {
+    $(this).parents('.form-group').addClass('focused');
+  });
+
+  $('input').blur(function () {
+    var inputValue = $(this).val();
+    if (inputValue == "") {
+      $(this).removeClass('filled');
+      $(this).parents('.form-group').removeClass('focused');
+    } else {
+      $(this).addClass('filled');
+    }
+  });
+
+  //disable context
+  $(document).bind("contextmenu", function (e) {
+    return false;
+  });
+
+  //validate email
+  var email = $('#email');
+
+  function isEmail(email) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,6})+$/;
+    return regex.test(email);
+  };
+
+
+  // validate Terms agreement
+  var terms = $('#terms');
+  var news = $('#news');
+  var termsSubmitLabel = $('#termsSubmitLabel');
+  var termsSubmit = $('#termsSubmit');
+
+  var email_terms_validation = function () {
+    if ($(terms).is(':checked') && (isEmail(email.val()) || email.val() === '')) {
+      termsSubmit.removeAttr('disabled');
+      termsSubmitLabel.removeClass('disabled');
+    } else {
+      termsSubmit.attr('disabled', 'disabled');
+      termsSubmitLabel.addClass('disabled');
+    }
+  };
+
+  terms.click(email_terms_validation);
+  news.click(email_terms_validation);
+
+  email.change(function () {
+    email_terms_validation()
+  });
+
+  /* setup modal */
+  var termsBtn = $('.terms-btn');
+  var policyBtn = $('.policy-btn');
+  var informationProvided = $('.information-provided');
+  var termsModal = $('#modal-terms');
+  var policyModal = $('#modal-policy');
+  var modalInformation = $('#modal-information');
+  var closeBtn = $('.ui-close-modal');
+
+  termsBtn.on('click', function () {
+    termsModal.addClass('show');
+  });
+
+  policyBtn.on('click', function () {
+    policyModal.addClass('show');
+  });
+
+  informationProvided.on('click', function () {
+    modalInformation.addClass('show');
+  });
+
+  closeBtn.on('click', function () {
+    termsModal.removeClass('show');
+    policyModal.removeClass('show');
+    modalInformation.removeClass('show');
+  });
+
+  // close modal by clicking outside the modal window
+  $('.modal-wrap').click(function (e) {
+    if (e.target === $('.modal-wrap.show')[0]) {
+      $('.modal-wrap').removeClass('show');
+    }
+  })
+
+  /* end modal */
+});
